85a1fdca410101b065cb98c14a86aad89b3935b2
Nishant Sinha <nishant813@gmail.com>
2021-04-14 00:44:06 -0700

    Improved responsiveness of itinerary pages

---
+++ itinerary-functions.js
@@ -0,0 +1,40 @@
+function changeImage(newImage) {
+	let image = document.querySelector("img");
+	let old_width = image.width;
+	let old_height = image.height;
+
+	image.src = newImage;
+	image.width = old_width;
+	image.height = old_height;
+}
+
+let destinations = document.querySelector("#destinations").children;
+for (let i=0; i < destinations.length; i++) {
+	destinations[i].onmouseover = function() {
+		changeImage(destinations[i].dataset.image);
+	}
+}
+
+$(".star").click(function(event) {
+	if ($(this).hasClass("bi-star")) {
+		$(this).removeClass("bi-star");
+		$(this).addClass("bi-star-fill");
+	}
+	else {
+		$(this).removeClass("bi-star-fill");
+		$(this).addClass("bi-star");
+	}
+});
+
+$(".arrow").click(function(event) {
+	event.stopPropagation();
+	// If restaurant not upvoted
+	if ($(this).hasClass("bi-arrow-up-circle")) {
+		$(this).removeClass("bi-arrow-up-circle");
+		$(this).addClass("bi-arrow-up-circle-fill");
+	}
+	else {
+		$(this).removeClass("bi-arrow-up-circle-fill");
+		$(this).addClass("bi-arrow-up-circle");
+	}
+});
